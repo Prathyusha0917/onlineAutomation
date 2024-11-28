32f7e1b3b32f8ef86f37524beb1b6aa2fb9fb59d
Prathyusha0917 <prathyusha0917@gmail.com>
2024-11-28 17:26:03 +0530

    suuu

---
+++ src/test/java/com/selenium/com/LaunchBrowser.java
@@ -0,0 +1,14 @@
+package com.selenium.com;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+
+	}
+
+}
